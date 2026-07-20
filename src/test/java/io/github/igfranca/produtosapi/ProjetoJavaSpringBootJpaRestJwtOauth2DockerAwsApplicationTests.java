367aa2c2237b6c77f26610ae1e3ff82609a83832
Igor Franca <igrfranca@gmail.com>
2026-07-20 12:28:01 -0300

    First Commit

---
+++ src/test/java/io/github/igfranca/produtosapi/ProjetoJavaSpringBootJpaRestJwtOauth2DockerAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.igfranca.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaSpringBootJpaRestJwtOauth2DockerAwsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
